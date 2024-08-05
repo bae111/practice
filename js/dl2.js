4c022c0f0e67bb2ecc2e0dbb5b99df04690ba886
bae111 <sunmoneee@gmail.com>
2024-08-05 16:48:19 +0900

    feat:dl 복습

---
+++ js/dl2.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('dl dt').on('click', function(){
+        $('+dd', this).slideDown(800).siblings('dd').slideUp(800)
+    })
+    $('dl dd').on('click', function(){
+        $(this).slideUp(800)
+    })
+})
